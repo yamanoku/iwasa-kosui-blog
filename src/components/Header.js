bc0c11354501d45435ae7c5794c599734301b1fd
Kosui Iwasa <iwasa@katfuji.lab.tuat.ac.jp>
2019-01-19 04:44:25 +0900

    Redesign Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,18 @@ import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
 import Image from 'gatsby-image'
 import styled from 'styled-components'
-import { FaTwitter } from 'react-icons/fa'
+import { FaTwitter, FaRss, FaGithub } from 'react-icons/fa'
 
 import { rhythm } from '../utils/typography'
 
 const H1 = styled.h1`
   margin: 0;
+  margin-bottom: 8px;
+  font-size: 1.5rem;
   @media (max-width: 768px) {
     font-size: 7vw;
   }
+  text-align: center;
 `
 
 class Header extends React.Component {
@@ -18,22 +21,50 @@ class Header extends React.Component {
     return (
       <div
         style={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
           marginBottom: '1.75rem',
         }}
       >
         <H1>ebiebievidence.com</H1>
-        <div>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+          }}
+        />
+        <div
+          style={{
+            height: 20,
+            marginBottom: 5,
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+        >
           <a
-            href="https://twitter.com/ebiebievidence"
+            href="https://twitter.com/ebiebievidence2"
             target="_blank"
             style={{
               boxShadow: 'none',
             }}
           >
-            <FaTwitter />
+            <FaTwitter style={{ marginRight: '16px', fontSize: '1.5rem' }} />
+          </a>
+          <a
+            href="/rss.xml"
+            target="_blank"
+            style={{
+              boxShadow: 'none',
+            }}
+          >
+            <FaRss style={{ marginRight: '16px', fontSize: '1.5rem' }} />
+          </a>
+          <a
+            href="https://github.com/EbiEbiEvidence"
+            target="_blank"
+            style={{
+              boxShadow: 'none',
+            }}
+          >
+            <FaGithub style={{ marginRight: '16px', fontSize: '1.5rem' }} />
           </a>
         </div>
       </div>
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -10,29 +10,7 @@ class Layout extends React.Component {
     const rootPath = `${__PATH_PREFIX__}/`
     let header
 
-    if (location.pathname === rootPath) {
-      header = <Header />
-    } else {
-      header = (
-        <h3
-          style={{
-            fontFamily: `Montserrat, sans-serif`,
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h3>
-      )
-    }
+    header = <Header />
     return (
       <div
         style={{
