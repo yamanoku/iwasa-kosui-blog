490324be8aaee40e4a66abc3ea84dae2ef135b1c
Kosui Iwasa <iwasa@katfuji.lab.tuat.ac.jp>
2019-01-20 01:58:56 +0900

    Refine typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,17 +1,86 @@
 import Typography from 'typography'
-import Wordpress2016 from 'typography-theme-wordpress-2016'
+import {
+  MOBILE_MEDIA_QUERY,
+  TABLET_MEDIA_QUERY,
+} from 'typography-breakpoint-constants'
+import gray from 'gray-percentage'
 
-Wordpress2016.overrideThemeStyles = () => {
-  return {
-    'a.gatsby-resp-image-link': {
-      boxShadow: `none`,
+const typography = new Typography({
+  title: 'Sutro',
+  baseFontSize: '16px',
+  baseLineHeight: 1.78 * 1.1,
+  googleFonts: [
+    {
+      name: 'M PLUS Rounded 1c',
+      styles: ['400', '700'],
     },
-  }
-}
-
-delete Wordpress2016.googleFonts
-
-const typography = new Typography(Wordpress2016)
+  ],
+  headerFontFamily: ['M PLUS Rounded 1c', 'sans-serif'],
+  bodyFontFamily: ['M PLUS Rounded 1c', 'sans-serif'],
+  bodyColor: 'hsla(0,0%,0%,0.9)',
+  headerWeight: 700,
+  bodyWeight: 400,
+  boldWeight: 700,
+  overrideStyles: ({ adjustFontSizeTo, scale, rhythm }, options) => ({
+    a: {
+      color: '#2358ce',
+      textDecoration: 'none',
+    },
+    'a:hover,a:active': {
+      textDecoration: 'underline',
+    },
+    blockquote: {
+      ...scale(1 / 5),
+      color: gray(41),
+      fontStyle: 'italic',
+      paddingLeft: rhythm(13 / 16),
+      marginLeft: 0,
+      borderLeft: `${rhythm(3 / 16)} solid ${gray(80)}`,
+    },
+    'blockquote > :last-child': {
+      marginBottom: 0,
+    },
+    'blockquote cite': {
+      ...adjustFontSizeTo(options.baseFontSize),
+      color: options.bodyColor,
+      fontWeight: options.bodyWeight,
+    },
+    'blockquote cite:before': {
+      content: '"— "',
+    },
+    ul: {
+      listStyle: 'disc',
+    },
+    'ul,ol': {
+      marginLeft: 0,
+    },
+    [MOBILE_MEDIA_QUERY]: {
+      'ul,ol': {
+        marginLeft: rhythm(1),
+      },
+      blockquote: {
+        marginLeft: rhythm(-3 / 4),
+        marginRight: 0,
+        paddingLeft: rhythm(9 / 16),
+      },
+    },
+    [TABLET_MEDIA_QUERY]: {
+      h1: {
+        ...scale(5 / 5),
+      },
+    },
+    'h1,h2,h3,h4,h5,h6': {
+      marginTop: rhythm(2),
+    },
+    h1: {
+      ...scale(6 / 5),
+      letterSpacing: '-2px',
+    },
+    h6: {
+      fontStyle: 'italic',
+    },
+  }),
+})
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
