59796485e9911b5ccd30ae56142d8c3644c68a5d
Kosui Iwasa <github@iwasakosui.com>
2021-09-26 22:27:40 +0900

    feat: add rss

---
+++ pages/rss.tsx
@@ -0,0 +1,38 @@
+import fs from 'fs';
+import RSS from 'rss';
+import { fetchAllPostsFromLocal } from '../lib/infra/post/local';
+import { fetchLatestPostList } from '../lib/value/post';
+
+
+export const getStaticProps = async () => {
+    const xml = await generateFeedXml();
+    await fs.writeFile(`${process.cwd()}/public/rss.xml`, xml, () => {});
+    return {
+        props: {},
+    };
+};
+
+const Page = () => null;
+export default Page;
+
+async function generateFeedXml() {
+    const feed = new RSS({
+        title: "ebiebievidence.com",
+        description: "ebiebievidence のブログ",
+        site_url: "https://www.ebiebievidence.com",
+        feed_url: "https://www.ebiebievidence.com/rss.xml",
+        language: 'ja',
+    });
+
+    const posts = await fetchLatestPostList([fetchAllPostsFromLocal]);
+    posts?.forEach((post) => {
+        feed.item({
+            title: post.title,
+            description: post.description,
+            date: new Date(post.createdAt),
+            url: `https://www.ebiebievidence.com/posts/${post.directory}/`,
+        });
+    })
+
+    return feed.xml();
+}
